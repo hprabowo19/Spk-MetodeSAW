ea9d0c9de5af177d994cbbe0db5dba418a00819b
LAPTOP-PU5L77UC\Hendri Prabowo <prabowo.sun3@gmail.com>
2018-07-25 03:17:23 +0700

    First Commit - Spk-MetodeSAW

--- configurasi/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "saw";
-set_time_limit(1800);
-mysql_connect($server,$user,$password) or die ("Koneksi gagal");
-mysql_select_db($database) or die ("Database tidak ditemukan");
-?>
